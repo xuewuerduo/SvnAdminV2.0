b5df0b254e79acaea791d873f3d5badc206f70f7
xuewuerduo <53554701+xuewuerduo@users.noreply.github.com>
2024-04-13 01:14:21 +0800

    Update start.sh

--- 03.cicd/svnadmin_docker/start.sh
+++ 03.cicd/svnadmin_docker/start.sh
@@ -2,7 +2,7 @@
 
 source /etc/profile
 
-/usr/bin/php-fpm
+/usr/local/sbin/php-fpm
 
 /usr/bin/svnserve --daemon --pid-file=/home/svnadmin/svnserve.pid -r '/home/svnadmin/rep/' --config-file '/home/svnadmin/svnserve.conf' --log-file '/home/svnadmin/logs/svnserve.log' --listen-port 3690 --listen-host 0.0.0.0
 
@@ -13,16 +13,17 @@ spid=$(uuidgen)
 ps aux | grep -v grep | grep "$spid" | awk 'NR==1' | awk '{print $2}' > '/home/svnadmin/sasl/saslauthd.pid'
 chmod 777 /home/svnadmin/sasl/saslauthd.pid
 
-/usr/sbin/crond
+/usr/sbin/cron
 
 /usr/sbin/atd
 
 /usr/bin/php /var/www/html/server/svnadmind.php start &
 
-rm -rf /run/httpd
-mkdir -p /run/httpd
-chown -R apache:apache /run/httpd
-/usr/sbin/httpd
+rm -rf /run/apache2
+mkdir -p /run/apache2
+#chown -R apache:apache /run/httpd
+chown -R www-data:www-data /run/httpd
+/usr/sbin/apache2
 
 while [[ true ]]; do
     sleep 1
